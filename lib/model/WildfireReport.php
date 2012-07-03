85f4797089749d628b86bf448ef1e1160f8d0549
charlesmarshall <cm56marshall@googlemail.com>
2012-07-03 12:48:03 +0100

    add in default value

--- lib/model/WildfireReport.php
+++ lib/model/WildfireReport.php
@@ -1,6 +1,6 @@
 <?
 class WildfireReport extends WaxModel{
-  public static $data_models = array();
+  public static $data_models = array(''=>'-- Select --');
   public function setup(){
     //if the cms exists, add the model to the array
     if(defined("CONTENT_MODEL")) WildfireReport::$data_models[CONTENT_MODEL] = "Content";
@@ -8,7 +8,7 @@ class WildfireReport extends WaxModel{
     $this->define("title", "CharField", array('required'=>true, 'default'=>'TITLE'));
     $this->define("data_model", "CharField", array('widget'=>'SelectInput', 'choices'=>WildfireReport::$data_models));
     $this->define("graphs", "ManyToManyField", array("eager_loading"=>true, "join_model_class"=>"WildfireOrderedTagJoin", "join_order"=>"join_order", 'target_model'=>'WildfireGraph', 'group'=>'relationships'));
-    
+
     parent::setup();
   }
 
