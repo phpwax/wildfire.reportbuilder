c3165431c337f05017589df06dab1cad99bf8204
charlesmarshall <cm56marshall@googlemail.com>
2012-07-03 12:45:03 +0100

    include the content model constant incase other plugins that set it arent in use

--- setup.php
+++ setup.php
@@ -1,8 +1,10 @@
 <?php
-
-
 CMSApplication::register_module("graph", array("display_name"=>"Graphs", "link"=>"/admin/graph/", 'plugin_name'=>'wildfire.reportbuilder', 'assets_for_cms'=>true));
 CMSApplication::register_module("report", array("display_name"=>"Reports", "link"=>"/admin/report/", 'split'=>true));
 
+if(!defined("CONTENT_MODEL")){
+  $con = new ApplicationController(false, false);
+  define("CONTENT_MODEL", $con->cms_content_class);
+}
 
 ?>
