00781167ebd50eca4bf5a0e836599e7b470d5b39
charlesmarshall <cm56marshall@googlemail.com>
2012-07-03 16:47:56 +0100

    working logic on the more complext graph types

--- lib/controller/CMSAdminReportsController.php
+++ lib/controller/CMSAdminReportsController.php
@@ -43,11 +43,11 @@ class CMSAdminReportsController extends AdminComponent{
   }
 
   public function simple_metric($data, $graph){
-    list($primary_metric, $secondary_metric, $primary_name, $secondary_name) = $this->parse_metric_columns($graph, $data);
-    $parsed = array(array($primary_name, $secondary_name));
+    $info = $this->parse_metric_columns($graph, $data);
+    $parsed = array(array($info['primary_name'], $info['secondary_name']));
     //group by the primary & also include it in the return
     $results = $data->group($primary_metric);
-    $results->select_columns = array_merge(array($data->model->primary_key), array($primary_metric ." AS primary_metric"), array($secondary_metric ." AS secondary_metric"));
+    $results->select_columns = array_merge(array($data->model->primary_key), array($info['primary_metric'] ." AS primary_metric"), array($info['secondary_metric'] ." AS secondary_metric"));
     $results = $results->order($graph->order_results)->all();
 
     foreach($results as $res) $parsed[] = array($res->row['primary_metric'], $res->row['secondary_metric']);
@@ -57,30 +57,57 @@ class CMSAdminReportsController extends AdminComponent{
 
   public function complex_metric($data, $graph){
     $parsed = array();
-    list($primary_metric, $secondary_metric, $primary_name, $secondary_name) = $this->parse_metric_columns($graph, $data);
-
-    $titles = array($primary_name=>1);
+    $info = $this->parse_metric_columns($graph, $data);
+    $titles = array($info['primary_name']);
 
     //so no grouping on this
-    $results = $data->group($primary_metric.", ". $secondary_metric);
-    $results->select_columns = array_merge(array($data->model->primary_key), array("count(*) as cnt"), array($primary_metric ." AS primary_metric"), array($secondary_metric ." AS secondary_metric"));
+    $results = $data->group($info['primary_metric'].", ". $info['secondary_col']);
+    $results->select_columns = array_merge(array($data->model->primary_key), array("count(*) as cnt"), array($info['primary_metric'] ." AS primary_metric"), array($info['secondary_col'] ." AS secondary_metric"));
     $results = $results->order($graph->order_results)->all();
 
+    $class = get_class($data->model);
+    $rows = array();
     foreach($results as $r){
-      $titles[$r->row['secondary_metric']] =1;
+      $p = $r->row['primary_metric'];
+      $s = $r->row['secondary_metric'];
+      if($s){
+        $model = new $class($r->row[$data->model->primary_key]);
+        if($val = $model->humanize($info['secondary_metric'])) $titles[$s] = $val;
+        else $titles[$s] = "n/a";
+        $rows[$p][$s] = $r->row['cnt'];
+      }
     }
-    print_r($titles);
+
+    $parsed[0] = array_values($titles);
+    $i=1;
+    foreach($rows as $key=>$r){
+      $parsed[$i][] = $key;
+      foreach($titles as $id=>$t){
+        if($r[$id]) $parsed[$i][] = $r[$id];
+      }
+      if(count($parsed[$i]) < count($titles)) for($x=count($parsed[$i]); $x < count($titles); $x++) $parsed[$i][$x] = 0;
+      $i++;
+    }
 
     return $parsed;
-
-
-    exit;
   }
 
   public function parse_metric_columns($graph, $data){
     $primary_metric = stripslashes(str_replace("?", $graph->primary_metric_column, $graph->primary_metric_function));
     $secondary_metric = stripslashes($graph->secondary_metric_column);
 
+    $class = get_class($data->model);
+    $model = new $class;
+    $info = $model->columns[$graph->primary_metric_column];
+    if(!$primary_name = $info['label']) $primary_name = Inflections::humanize($graph->primary_metric_column);
+    if(($info = $model->columns[$graph->secondary_metric_column])){
+      if(!$secondary_name = $info['label']) $secondary_name = Inflections::humanize($graph->secondary_metric_column);
+      $col = $model->get_col($graph->secondary_metric_column);
+      $secondary_col = $col->col_name;
+    }else $secondary_name = $graph->secondary_metric_column;
+
+    $res = array('primary_metric'=>$primary_metric, 'secondary_metric'=>$secondary_metric, 'primary_name'=>$primary_name, 'secondary_name'=>$secondary_name, 'primary_col'=>$graph->primary_metric_column, 'secondary_col'=>$secondary_col);
+    return $res;
   }
 
 }
