66eb96fdfaa53ffc1bea4c3a4bf426f3653f06cf
charlesmarshall <cm56marshall@googlemail.com>
2012-07-03 12:45:15 +0100

    the report model

---
+++ lib/model/WildfireReport.php
@@ -0,0 +1,16 @@
+<?
+class WildfireReport extends WaxModel{
+  public static $data_models = array();
+  public function setup(){
+    //if the cms exists, add the model to the array
+    if(defined("CONTENT_MODEL")) WildfireReport::$data_models[CONTENT_MODEL] = "Content";
+
+    $this->define("title", "CharField", array('required'=>true, 'default'=>'TITLE'));
+    $this->define("data_model", "CharField", array('widget'=>'SelectInput', 'choices'=>WildfireReport::$data_models));
+    $this->define("graphs", "ManyToManyField", array("eager_loading"=>true, "join_model_class"=>"WildfireOrderedTagJoin", "join_order"=>"join_order", 'target_model'=>'WildfireGraph', 'group'=>'relationships'));
+    
+    parent::setup();
+  }
+
+}
+?>
