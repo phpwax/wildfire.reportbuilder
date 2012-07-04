66bd81c5c068e89f18f5f290f685fdc597ef0dd3
charlesmarshall <cm56marshall@googlemail.com>
2012-07-04 17:07:23 +0100

    add in the new size option to the graphs

--- lib/model/WildfireGraph.php
+++ lib/model/WildfireGraph.php
@@ -2,6 +2,7 @@
 class WildfireGraph extends WaxModel{
 
   public static $graph_types = array('ColumnChart'=>'Column', 'BarChart'=>'Bar', 'PieChart'=>'Pie', 'AreaChart'=>'Area');
+  public static $graph_sizes = array('small'=>'Small', 'medium'=>'Medium', 'large'=>'Large');
   public function setup(){
     $this->define("title", "CharField", array('required'=>true, 'default'=>'TITLE', 'scaffold'=>true));
     $this->define("type", "CharField", array('scaffold'=>true, 'widget'=>'SelectInput', 'choices'=>WildfireGraph::$graph_types));
@@ -10,6 +11,7 @@ class WildfireGraph extends WaxModel{
     $this->define("primary_metric_function", "CharField");
     $this->define("secondary_metric_column", "CharField", array('label'=>'Secondary metric'));
 
+    $this->define("size", "CharField", array('widget'=>'SelectInput', 'choices'=>WildfireGraph::$graph_sizes, 'default'=>'small'));
     parent::setup();
   }
 
